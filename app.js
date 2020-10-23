d5fdde094b4387a2d3a0ad726546337c32b65c3a
Tarakanov Dmitry <tarakanoffdmitry@yandex.ru>
2020-10-23 18:38:46 +0300

    a

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 });
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://tarakanov.students.nomoreparties.space/');
+  res.header('Access-Control-Allow-Origin', 'https://tarakanov.students.nomoreparties.space/*');
   next();
 });
 
