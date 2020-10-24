d02e0f80e1350e663f27623093990f3441d7bd61
Tarakanov Dmitry <tarakanoffdmitry@yandex.ru>
2020-10-24 17:34:24 +0300

    asd

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Credentials", true);
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  res.status(204).send();
   next();
 });
 app.use(bodyParser.json());
